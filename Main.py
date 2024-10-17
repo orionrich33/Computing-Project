ac9fba879943a0724af05b7727204d7e792a8f47
orionrich33 <lrupniak33@gmail.com>
2024-10-17 19:09:25 +0100

    Working

--- Main.py
+++ Main.py
@@ -6,14 +6,25 @@ m_m = 7.348*1e22               #Mass of Moon
 m_e = 5.972*1e24            #Mass of Earth
 r_e = 4670*1e3                       #Radius of Earth to C.O.M. in km
 r_m = 379728*1e3                 #Radius of Moon to C.O.M.
-i_r_pos = np.array([-300000,0])            #Initial position of rocket
-i_r_vel = np.array([0,0])            #Initial velocity of rocket
 G = 6.67*1e-11                   #Gravitational constant
-a = 500             #Time step 
-phi = 3           #phase
+a = 10             #Time step 
+phi = 0           #phase
+d = 3.844*1e8               #earth-moon distance
+
+#compute L2
+l2_delta_r = d*(m_m/(3*m_e))**(1/3)
+l2 = l2_delta_r+r_m 
+
+#compute T
+T = 2*np.pi*np.sqrt(((r_e+r_m)**3)/(G*(m_e+m_m)))
+
+#compute l2_v
+l2_v = (l2*2*np.pi)/T
 
 #create arrays
-N = 3000         #number of iterations
+i_r_pos = np.array([l2*1.1,0])            #Initial position of rocket
+i_r_vel = np.array([0,l2_v])            #Initial velocity of rocket
+N = 5000000         #number of iterations
 s = (N,2)
 s2 = (N+1,2)
 m_pos = np.zeros(s)
@@ -24,14 +35,10 @@ r_acc = np.zeros(s)
 r_pos[0] = i_r_pos
 r_vel[0] = i_r_vel
 
-#compute T
-T = 2*np.pi*np.sqrt(((r_e+r_m)**3)/(G*(m_e+m_m)))
-
-
 for i in range(0,N):
     theta = (2*np.pi*(i*a))/T
-    e_pos[i] = np.array([r_e*np.sin(theta), r_e*np.cos(theta)])
-    m_pos[i] = np.array([r_m*np.sin(theta+phi), r_m*np.cos(theta+phi)])
+    e_pos[i] = np.array([r_e*np.cos(theta), r_e*np.sin(theta)])
+    m_pos[i] = np.array([r_m*np.cos(theta+phi), r_m*np.sin(theta+phi)])
     de_x = r_pos[i][0]-e_pos[i][0]
     dm_x = r_pos[i][0]-m_pos[i][0]
     de_y = r_pos[i][1]-e_pos[i][1]
@@ -47,11 +54,16 @@ for i in range(0,N):
     r_pos_x = r_pos[i][0]+a*r_vel_x+(((a**2)/2)*r_acc_x)
     r_pos_y = r_pos[i][1]+a*r_vel_y+(((a**2)/2)*r_acc_y)
     r_pos[i+1] = [r_pos_x, r_pos_y]
+
+print(m_pos)
     
+plt.plot(r_pos[:,0], r_pos[:,1], color='red', label='rocket')
 plt.plot(e_pos[:,0], e_pos[:,1], color='blue', label='earth')
-plt.plot(r_pos[:12,0], r_pos[:12,1], color='red', label='rocket')
+plt.plot(m_pos[:,0], m_pos[:,1], color='green', label='moon')
 plt.legend()
 plt.show()
+
+
 
 
 
